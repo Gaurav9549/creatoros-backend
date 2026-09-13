02a16f3a405ecf227a6346d81b33557c87dc184a
thilakkkk <bakathilak@gmail.com>
2026-09-13 22:01:36 +0530

    Initial CreatorOS backend setup

---
+++ src/main/java/com/creatoros/backend/CreatorosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.creatoros.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creatoros/backend/CreatorosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatoros.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
